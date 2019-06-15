c0a9708c3606059d58c32fe0d6bc4bc95f29f4fd
BF <bf@xhs.technology>
2019-06-15 15:12:29 +1000

    Updates character selection table to show names and images.

--- Crossing Paths/Crossing Paths/ChooseStartingCharacterViewController.swift
+++ Crossing Paths/Crossing Paths/ChooseStartingCharacterViewController.swift
@@ -8,9 +8,34 @@
 
 import UIKit
 
-class ChooseStartingCharacterViewController: UIViewController {
+class ChooseStartingCharacterTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var characterNameLabel: UILabel!
+    
+}
+
+class ChooseStartingCharacterViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    class CharacterData {
+        
+        let name: String
+        let image: UIImage
+        
+        init(name: String, image: UIImage) {
+            self.name = name
+            self.image = image
+        }
+    }
+    
 
     @IBOutlet weak var tableView: UITableView!
+    
+    let characters: [CharacterData] = [
+        CharacterData(name: "Jeff", image: UIImage(named: "cat")!),
+        CharacterData(name: "Kerber", image: UIImage(named: "bun")!)
+    ]
     
     
     override func viewDidLoad() {
@@ -18,18 +43,45 @@ class ChooseStartingCharacterViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
+        tableView.tableFooterView = UIView()
+        tableView.layoutMargins = UIEdgeInsets.zero
+        tableView.separatorInset = UIEdgeInsets.zero
+    }
+    
+    
+    
+    
+    
+    // MARK: - TableViewDelegate/DataSource Implementation
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell", for: indexPath) as! ChooseStartingCharacterTableViewCell
+        
+        let characterData = characters[indexPath.row]
+        
+        cell.characterNameLabel.text = characterData.name
+        cell.characterImageView.image = characterData.image
+        
+        cell.layoutMargins = UIEdgeInsets.zero
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let characterData = characters[indexPath.row]
+
+        
+        print("You selected \(characterData.name)")
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
