c9eda7ea4104f83be729f1c667129eb512ff735f
BF <bf@xhs.technology>
2019-06-20 20:14:46 +1000

    Adds material text field. Updates style of screens.

--- Crossing Paths/Crossing Paths/EnterRoomNameViewController.swift
+++ Crossing Paths/Crossing Paths/EnterRoomNameViewController.swift
@@ -8,27 +8,32 @@
 
 import UIKit
 import FirebaseDatabase
-
+import MaterialTextField
 
 protocol EnterRoomNameViewControllerDelegate: AnyObject {
-    
     func enterRoomName(vc: EnterRoomNameViewController, enteredRoomName: String)
-    
 }
 
 
-class EnterRoomNameViewController: UIViewController {
+class EnterRoomNameViewController: UIViewController, UITextFieldDelegate {
 
     weak var delegate: EnterRoomNameViewControllerDelegate? = nil
     
     @IBOutlet weak var roomNameTextField: UITextField!
     @IBOutlet weak var roomNameButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roomNameTextField.delegate = self
+    }
+    
     @IBAction func onButtonPressed(_ sender: Any) {
         
         guard let input = roomNameTextField.text else {
             return
         }
+        
+        self.view.endEditing(true)
         
         guard input.count > 0 else {
             self.displayError(text: "Please enter a room name")
@@ -40,7 +45,15 @@ class EnterRoomNameViewController: UIViewController {
     
     func displayError(text: String) {
         // TODO: Display the error here
-        print(text)
+        let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: text])
+        (roomNameTextField as? MFTextField)?.setError(error, animated: true)
     }
+    
+    // UITextFieldDelegate Implementation
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        (roomNameTextField as? MFTextField)?.setError(nil, animated: true)
+    }
+    
 
 }
