71f4b327b2275ddb221d5fb0de537dd0452ea15b
BF <bf@xhs.technology>
2019-06-30 10:36:42 +1000

    Now authenticates on Android.

--- CPVoter/app/src/main/java/com/tarobang/cpvoter/MainActivity.kt
+++ CPVoter/app/src/main/java/com/tarobang/cpvoter/MainActivity.kt
@@ -3,6 +3,9 @@ package com.tarobang.cpvoter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,10 @@ class MainActivity : AppCompatActivity() {
 
         val auth = FirebaseAuth.getInstance()
 
+        val x = auth.signInAnonymously().addOnCompleteListener {
+            val activityChangeIntent = Intent(this@MainActivity, EnterSessionActivity::class.java)
+            startActivity(activityChangeIntent)
+        }
 
 
     }
--- Crossing Paths/Crossing Paths/AppDelegate.swift
+++ Crossing Paths/Crossing Paths/AppDelegate.swift
@@ -21,6 +21,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Override point for customization after application launch.
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
         
         FirebaseApp.configure()
