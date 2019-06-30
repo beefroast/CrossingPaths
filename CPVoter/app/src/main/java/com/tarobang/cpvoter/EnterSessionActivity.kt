dcf4f895e02e1c6cd114313670d1bb6d3200d0ad
BF <bf@xhs.technology>
2019-06-30 10:47:37 +1000

    Setups the view for enter room screen.

--- CPVoter/app/src/main/java/com/tarobang/cpvoter/EnterSessionActivity.kt
+++ CPVoter/app/src/main/java/com/tarobang/cpvoter/EnterSessionActivity.kt
@@ -11,12 +11,11 @@ class EnterSessionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_session)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
 
 }
