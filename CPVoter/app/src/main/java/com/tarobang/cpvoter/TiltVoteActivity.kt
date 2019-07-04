1c2c81f17a1a5b251239c41e338e02cf408c5758
BF <bf@xhs.technology>
2019-07-04 19:23:48 +1000

    Tilt voting in Android should theoretically work now.

--- CPVoter/app/src/main/java/com/tarobang/cpvoter/TiltVoteActivity.kt
+++ CPVoter/app/src/main/java/com/tarobang/cpvoter/TiltVoteActivity.kt
@@ -7,6 +7,11 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorManager
 import android.hardware.SensorEventListener
+import android.opengl.Visibility
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.math.PI
 import kotlin.math.acos
 import kotlin.math.sqrt
 
@@ -14,12 +19,21 @@ class TiltVoteActivity : BaseActivity(), SensorEventListener {
 
     var vote: Vote = Vote.NONE
 
+    lateinit var textView: TextView
+    lateinit var arrow: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tilt_vote)
 
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+
+        textView = findViewById(R.id.tilt_text_view)
+        arrow = findViewById(R.id.arrow_image_view)
+
+        this.arrow.visibility = View.INVISIBLE
+        this.textView.visibility = View.VISIBLE
 
         sensor?.let { sensor ->
             sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME)
@@ -47,9 +61,13 @@ class TiltVoteActivity : BaseActivity(), SensorEventListener {
 
         if (z <= 0.0 && z >= -sqrt(0.5) == false) {
 
-            // TODO: Handle not being on the correct tilt
+            this.arrow.visibility = View.INVISIBLE
+            this.textView.visibility = View.VISIBLE
             updateVote(Vote.NONE)
         }
+
+        this.textView.visibility = View.GONE
+
 
         // Ignore the Z component and normalize the (X,Y) Vector
 
@@ -66,13 +84,17 @@ class TiltVoteActivity : BaseActivity(), SensorEventListener {
         if (radians <= 0.3) {
             // TODO: Bail out they're not voting
             updateVote(Vote.NONE)
+            this.arrow.visibility = View.INVISIBLE
         }
-
 
         if (nX < 0.0) {
             updateVote(Vote.LEFT)
+            this.arrow.visibility = View.VISIBLE
+            this.arrow.rotation = (radians * PI * 0.5).toFloat()
         } else {
             updateVote(Vote.RIGHT)
+            this.arrow.visibility = View.VISIBLE
+            this.arrow.rotation = (radians * PI * 0.5).toFloat()
         }
     }
 
