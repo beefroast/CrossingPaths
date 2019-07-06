ccfcfdb02e9a75ec0ad8d4370e474e788e14f90b
BF <bf@xhs.technology>
2019-07-06 12:38:00 +1000

    Can now go back to select room screen from credits.

--- CPVoter/app/src/main/java/com/tarobang/cpvoter/CreditActivity.kt
+++ CPVoter/app/src/main/java/com/tarobang/cpvoter/CreditActivity.kt
@@ -2,13 +2,21 @@ package com.tarobang.cpvoter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class CreditActivity : AppCompatActivity() {
+class CreditActivity : BaseActivity() {
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credit)
+
+        val creditsButton = findViewById<Button>(R.id.end_credits_button)
+
+        creditsButton.setOnClickListener {
+            FirebaseManager.instance.goBackToRoomEntryScreen()
+        }
+
     }
 }
